e8abb4fe47e8409cdbc08a14309a9f8d55537fee
misterVKLN <misterVKLN@gmail.com>
2025-01-30 15:18:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "🚀 Запуск Docker Daemon..."
-if [ -e /var/run/docker.pid ]; then
-    rm /var/run/docker.pid
-fi
-nohup dockerd > /dev/null 2>&1 &
-sleep 10
-
-echo "🎓 Запуск Open edX..."
+echo "🚀 Запускаем Open edX..."
 tutor local start
-
-echo "✅ Open edX запущен! Система готова к работе."
-
-# Оставляем контейнер запущенным
-tail -f /dev/null
